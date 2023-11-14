9b2552c1b37dd08cf878d2132edeb743eefbecd0
ns2pole <spaceofstar2@gmail.com>
2023-11-14 15:27:52 +0900

    first commit

---
+++ TimeTrackManager/src/main/java/com/example/TimeTrackManager/MainController.java
@@ -0,0 +1,19 @@
+package com.example.TimeTrackManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+
+    @GetMapping("/login")
+    public String index(Model model){
+        model.addAttribute("name", "変数の受け渡し成功");
+        return "login";
+    }
+
+}
---
+++ TimeTrackManager/src/main/java/com/example/TimeTrackManager/TimeTrackManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.TimeTrackManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackManagerApplication.class, args);
+	}
+
+}
